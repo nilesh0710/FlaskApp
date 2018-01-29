7c3c309e9e18d8971dc2b9a4b406bf7a33f8a1fc
Nilesh Kumar <knilesh1007@gmail.com>
2018-01-29 13:18:59 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Flask CRUD Application:
+
+1) Navigate to the project directory and then run:
+
+        pip install -r requirements.txt
+
+
+2) After installing the dependencies, run these commands in python from project directory:
+
+        from bookmanager import db
+        db.create_all()
+        exit()
+
+
+3)Now to run the application use this command:
+
+        python app.py
---
+++ app.py
@@ -0,0 +1,61 @@
+import os
+
+from flask import Flask, redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "movie.db"))
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+
+db = SQLAlchemy(app)
+
+
+class Movie(db.Model):
+    title = db.Column(db.String(128), unique=True, nullable=False, primary_key=True)
+
+    def __repr__(self):
+        return "<Title: {}>".format(self.title)
+
+
+@app.route('/', methods=["GET", "POST"])
+def home():
+    if request.form:
+        try:
+            movie = Movie(title=request.form.get("title"))
+            db.session.add(movie)
+            db.session.commit()
+        except Exception as e:
+            print("Failed to add movie")
+            print(e)
+    movies = Movie.query.all()
+    return render_template("home.html", movies=movies)
+
+
+@app.route("/update", methods=["POST"])
+def update():
+    try:
+        newtitle = request.form.get("newtitle")
+        oldtitle = request.form.get("oldtitle")
+        movie = Movie.query.filter_by(title=oldtitle).first()
+        movie.title = newtitle
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print("Couldn't update movie title")
+        print(e)
+    return redirect("/")
+
+
+@app.route("/delete", methods=["POST"])
+def delete():
+    title = request.form.get("title")
+    movie = Movie.query.filter_by(title=title).first()
+    db.session.delete(movie)
+    db.session.commit()
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
